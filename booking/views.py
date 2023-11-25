fad771d1fdecd69f5ac096001b4352d66e6b6189
kjh <jh2two@gmail.com>
2023-11-25 12:56:18 +0900

    add APIViews

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Booking
+from .serializers import BookingSerializer
 
 # Create your views here.
+# API 뷰
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all();
+    serializer_class = BookingSerializer
+
+# API 뷰
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
